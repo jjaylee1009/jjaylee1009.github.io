2d51c8def39955047bf7ebff509526a16497f5b9
jjaylee1009 <144849517+jjaylee1009@users.noreply.github.com>
2023-10-17 00:33:29 -0400

    add jquery

---
+++ letterform_motion/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $(".button").click(function(){
+      $(".wonegroup").toggle();
+      $(".onecode").toggle();
+    });
+  });
+
+$(document).ready(function(){
+    $(".buttontwo").click(function(){
+      $(".wtwogroup").toggle();
+      $(".twocode").toggle();
+    });
+  });
+
+$(document).ready(function(){
+    $(".buttonthree").click(function(){
+      $(".wthreegroup").toggle();
+      $(".threecode").toggle();
+    });
+  });
+
+$(document).ready(function(){
+    $(".buttonfour").click(function(){
+      $(".wfourgroup").toggle();
+      $(".fourcode").toggle();
+    });
+  });
+
+$(document).ready(function(){
+    $(".buttonfive").click(function(){
+      $(".wfivegroup").toggle();
+      $(".fivecode").toggle();
+    });
+  });
